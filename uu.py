4802691d2190850f50e98819b28874fcf8e5892f
NUML <user@SE-LAB1-16.numlit.edu.pk>
2021-03-20 14:45:38 +0500

    hello world

---
+++ uu.py
@@ -0,0 +1,17 @@
+from numpy import *
+import numpy 
+from pandas import *
+import pandas as pd
+from matplotlib import *
+from matplotlib import pyplot as plt
+# data = pd.read_csv('countries.csv')
+# pak = data[data.country == 'Pakistan']
+# bah = data[data.country == 'Bahrain']
+# plt.plot(pak.year, pak.population)
+# plt.plot(bah.year, bah.population)
+# plt.title('Graph')
+# plt.xlabel("x-values")
+# plt.ylabel("y-values")
+# plt.legend(["Column A - B", "Column A - C"])
+# plt.show()
+#
